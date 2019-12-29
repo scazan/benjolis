54080d0d37f42663205da198a1edc4455ddf275f
Scott Cazan <scottcazan@gmail.com>
2019-12-30 00:29:10 +0100

    cleanup code and add shift features

--- benjolis.lua
+++ benjolis.lua
@@ -2,29 +2,30 @@
 --
 -- A norns version of Alejandro Olarte's Benjolis SC patch
 -- Instrument inspired from Rob Hordijk's Benjolin, it requires sc3-plugins (PulseDPW, SVF and DFM1)
--- 
+--
 -- UI:
 -- Use buttons 2 and 3 to
 -- cycle through pairs of dials.
--- 
+--
 -- Use encoders 2 and 3 to
 -- adjust the left and right
 -- dials of the selected
 -- pair.
--- 
+--
 -- Use encoder 1 to 
 -- control volume.
--- 
+--
 -- If you hold button 1 (shift)
 -- and use button 2, you will
 -- have a momentary mute.
--- 
+--
 -- If you hold button 1 (shift)
 -- and use button 3, you will
 -- have a mute that toggles.
 --
 -- Thanks to Alejandro Olarte
 -- for the SynthDef
+-- Norns version: @scazan
 
 
 local UI = require "ui"
@@ -34,11 +35,6 @@ engine.name = 'Benjolis'
 local dials = {}
 local numDials = 12
 
-local dialValues = {
-  0, 0,   0, 0,   0, 0,
-  0, 0,   0, 0,   0, 0,
-}
-
 local controlPairIndex = 1
 local paramsInList = {}
 
@@ -47,22 +43,55 @@ local screen_refresh_metro
 local screen_dirty = true
 
 function init()
-  -- show engine commands available
-  engine.list_commands()
-  
+  -- screen: turn on anti-alias
+  screen.aa(1)
+  screen.line_width(1.0)
+
+  -- IDs and short names
+  paramsInList = {
+    {"setFreq1", "f1", "hz"},
+    {"setFreq2", "f2", "hz"},
+    {"setFiltFreq", "flt", "hz"},
+    {"setFilterType", "typ", ""},
+    {"setRungler1", "r1", "hz"},
+    {"setRungler2", "r2", "hz"},
+    {"setRunglerFilt", "rflt", "hz"},
+    {"setQ", "Q", ""},
+    {"setScale", "scl", ""},
+    {"setOutSignal", "out", ""},
+    {"setLoop", "loop", ""},
+    {"setAmp", "vol", ""},
+  }
+
+  -- add parameters from the engine
+  addParams()
+
+  -- GUI
+  addDials()
+
+  -- Start drawing to screen
+  screen_refresh_metro = metro.init()
+  screen_refresh_metro.event = function()
+    if screen_dirty then
+      screen_dirty = false
+      redraw()
+    end
+  end
+  screen_refresh_metro:start(1 / SCREEN_FRAMERATE)
+end
+
+function addParams()
   params:add{type = "number", id = "midi_device", name = "MIDI Device", min = 1, max = 4, default = 1, action = function(value)
     midi_in_device.event = nil
     midi_in_device = midi.connect(value)
     midi_in_device.event = midi_event
   end}
-  
-  local spec = ControlSpec.new(0, 1, "lin", 0, 0.5, "");
+
   local channels = {"All"}
   for i = 1, 16 do table.insert(channels, i) end
   params:add{type = "option", id = "midi_channel", name = "MIDI Channel", options = channels}
-  
   params:add_separator()
-  
+
   params:add{type = "control", controlspec = ControlSpec.new( 20.0, 14000.0, "exp", 0, 70, "Hz"), id = "setFreq1", name = "frequency 1", action = engine.setFreq1}
   params:add{type = "control", controlspec = ControlSpec.new( 0.1, 14000.0, "exp", 0, 4, "Hz"), id = "setFreq2", name = "frequency 2", action = engine.setFreq2}
   params:add{type = "control", controlspec = ControlSpec.new( 20.0, 20000.0, "exp", 0, 40, "Hz"), id = "setFiltFreq", name = "filter frequency", action = engine.setFiltFreq}
@@ -82,92 +111,87 @@ function init()
   params:add{type = "control", controlspec = ControlSpec.new( 0.0, 6.0, "lin", 1, 6), id = "setOutSignal", name = "out signal", action = engine.setOutSignal}
   params:add{type = "control", controlspec = ControlSpec.new( 0.0, 6.0, "lin", 1, 6), id = "setGain", name = "gain", action = engine.setGain}
   params:add{type = "control", controlspec = ControlSpec.new( 0.0, 1.0, "lin", 0, 1), id = "setAmp", name = "amp", action = engine.setAmp}
-  
-  -- screen: turn on anti-alias
-  screen.aa(1)
-  screen.line_width(1.0)
-  
-  -- IDs and short names
-  paramsInList = {
-      {"setFreq1", "f1", "hz"},
-      {"setFreq2", "f2", "hz"},
-      {"setFiltFreq", "flt", "hz"},
-      {"setFilterType", "typ", ""},
-      {"setRungler1", "r1", "hz"},
-      {"setRungler2", "r2", "hz"},
-      {"setRunglerFilt", "rflt", "hz"},
-      {"setQ", "Q", ""},
-      {"setScale", "scl", ""},
-      {"setOutSignal", "out", ""},
-      {"setLoop", "loop", ""},
-      {"setAmp", "vol", ""},
-  }
-  
-  -- GUI
-  addDials()
-  
-  -- Start drawing to screen
-  screen_refresh_metro = metro.init()
-  screen_refresh_metro.event = function()
-    if screen_dirty then
-      screen_dirty = false
-      redraw()
-    end
-  end
-  screen_refresh_metro:start(1 / SCREEN_FRAMERATE)
-  
 end
-  
+
+
 function addDials()
   -- make all the dials
   local dialSpacer = 20
   local row = 0
   local numColumns = 6
-  
+
   for i=1,numDials do
     local xOffset = 5 + (dialSpacer * ((i-1) % numColumns)) 
     local yOffset = 5 + (row * 30)
     local extraSpace = 0;
-    
+
     if ((i % 2) == 1) then
       xOffset = xOffset + 5
     end
-    
-    dials[i] = UI.Dial.new(xOffset, yOffset, 12, dialValues[i], 0, 1, 0.01, 0, nil, paramsInList[i][3], paramsInList[i][2])
-    
+
+    dials[i] = UI.Dial.new(xOffset, yOffset, 12, params:get_raw(paramsInList[i][0]), 0, 1, 0.01, 0, nil, paramsInList[i][3], paramsInList[i][2])
+
     if ((i % numColumns) == 0) then
       row = row + 1
       end
   end
 end
-    
+
+function setParam(paramID, dialGroupIndex, deltaValue)
+  params:set_raw(paramID, params:get_raw(paramID) + deltaValue)
+  dials[dialGroupIndex]:set_value(params:get_raw(paramID))
+end
+
 -- encoder function
 function enc(n, delta)
     local dialGroupIndex = (controlPairIndex * 2) - 1
-    
+    local deltaValue = util.clamp(delta, -0.01, 0.01)
+
     if n == 1 then
-      dialValues[12] = util.clamp(dialValues[12] + util.clamp(delta, -0.01, 0.01), 0, 1)
-      dials[12]:set_value(dialValues[12])
-      params:set_raw("setAmp", dialValues[12])
+      -- TODO: Should I use delta()
+      params:set_raw("setAmp", util.clamp(params:get_raw("setAmp") + deltaValue, 0, 1))
+      dials[12]:set_value(params:set_raw("setAmp"))
     elseif n == 2 then
-      dialValues[dialGroupIndex] = util.clamp(dialValues[dialGroupIndex] + util.clamp(delta, -0.01, 0.01), 0, 1)
-      dials[dialGroupIndex]:set_value(dialValues[dialGroupIndex])
-      params:set_raw(paramsInList[dialGroupIndex][1], dialValues[dialGroupIndex])
+      local paramID = paramsInList[dialGroupIndex][1]
+
+      setParam(paramID, dialGroupIndex, deltaValue)
     elseif n == 3 then
-      dialGroupIndex = dialGroupIndex + 1 
-      
-      dialValues[dialGroupIndex] = util.clamp(dialValues[dialGroupIndex] + util.clamp(delta, -0.01, 0.01), 0, 1)
-      dials[dialGroupIndex]:set_value(dialValues[dialGroupIndex])
-      params:set_raw(paramsInList[dialGroupIndex][1], dialValues[dialGroupIndex])
+      local paramID = paramsInList[dialGroupIndex+1][1]
+
+      setParam(paramID, dialGroupIndex, deltaValue)
     end
-      
+
   screen_dirty = true
 end
 
+local stashedVol = 0
+local muted = false
+local shift = 0
 -- key function
 function key(n, z)
-  if (shift == 1) then
+  if (n == 1) then
+    shift = z
+  elseif (shift == 1) then
     print("shift", n, z)
+    if (n == 2) then
+      -- momentary mute state
+      if (z == 1) then
+        stashedVol = params:get_raw("setAmp")
+        params:set_raw("setAmp", 0)
+      else
+        params:set_raw("setAmp", stashedVol)
+        stashedVol = 0
+      end
+    elseif ((n == 3) and (z == 0)) then
+      -- togglable mute state
+      if (muted) then
+        params:set_raw("setAmp", stashedVol)
+        stashedVol = 0
+      else
+        stashedVol = params:get_raw("setAmp")
+        params:set_raw("setAmp", 0)
+      end
+    end
   else
     if ((z == 1) and (n == 2)) then
       controlPairIndex = util.clamp(controlPairIndex - 1, 1, 6)
@@ -184,36 +208,18 @@ end
 function redraw()
   -- clear screen
   screen.clear()
-  
   -- set pixel brightness (0-15)
   screen.level(1)
-  
-  local indexToDraw = 1
-  local markerXOffset = 21 + ((controlPairIndex-1) * 40)
+
+  -- draw Dials
   for i=1,numDials do
     dials[i]:redraw()
     dials[i].active = false
-    
-    if (indexToDraw == controlPairIndex) then
-      local rowIndex = 1
-      local markerXCurrentOffset = markerXOffset
-      if (controlPairIndex > 3) then
-        rowIndex = 2
-        markerXCurrentOffset = markerXOffset - ((4-1) * 40)
-      end
-    --  screen.level(15)
-     -- screen.move(markerXCurrentOffset, 4 + ((rowIndex-1) * 30))
-      --screen.text("▼")
-    end
-    
-    
-    if (i%2 == 1) then
-      indexToDraw = indexToDraw + 1
-    end
   end
-  
+
+  -- activate the currently selected pair
   dials[(controlPairIndex) * 2].active = true
   dials[(controlPairIndex * 2) - 1].active = true
-  
+
   screen.update()
 end
