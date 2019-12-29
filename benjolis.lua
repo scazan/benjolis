f6f2977fbf52bd5abc95d40c6642c05e18d253f0
Scott Cazan <scottcazan@gmail.com>
2019-12-29 22:05:43 +0100

    UI works!

--- benjolis.lua
+++ benjolis.lua
@@ -5,7 +5,12 @@ engine.name = 'Benjolis'
 local dials = {}
 local numDials = 12
 
-local listIndex = 4
+local dialValues = {
+  0, 0,   0, 0,   0, 0,
+  0, 0,   0, 0,   0, 0,
+}
+
+local listIndex = 1
 local paramsInList = {}
 
 local SCREEN_FRAMERATE = 15
@@ -47,6 +52,7 @@ function init()
   params:add_separator()
   params:add{type = "control", controlspec = ControlSpec.new( 0.0, 6.0, "lin", 1, 6), id = "setOutSignal", name = "out signal", action = engine.setOutSignal}
   params:add{type = "control", controlspec = ControlSpec.new( 0.0, 6.0, "lin", 1, 6), id = "setGain", name = "gain", action = engine.setGain}
+  params:add{type = "control", controlspec = ControlSpec.new( 0.0, 1.0, "lin", 0, 1), id = "setAmp", name = "amp", action = engine.setAmp}
   
   -- screen: turn on anti-alias
   screen.aa(1)
@@ -56,16 +62,16 @@ function init()
   paramsInList = {
       {"setFreq1", "f1", "hz"},
       {"setFreq2", "f2", "hz"},
-      {"setFilterFreq", "flt", "hz"},
+      {"setFiltFreq", "flt", "hz"},
       {"setFilterType", "typ", ""},
-      {"setLoop", "loop", ""},
-      {"", "", ""},
-      {"setRunglerFilt", "rflt", "hz"},
-      {"setQ", "Q", ""},
       {"setRungler1", "r1", "hz"},
       {"setRungler2", "r2", "hz"},
+      {"setRunglerFilt", "rflt", "hz"},
+      {"setQ", "Q", ""},
       {"setScale", "scl", ""},
       {"setOutSignal", "out", ""},
+      {"setLoop", "loop", ""},
+      {"setAmp", "vol", ""},
   }
   
   -- GUI
@@ -98,7 +104,7 @@ function addDials()
       xOffset = xOffset + 5
     end
     
-    dials[i] = UI.Dial.new(xOffset, yOffset, 12, 25, 0, 100, 1, 0, nil, paramsInList[i][3], paramsInList[i][2])
+    dials[i] = UI.Dial.new(xOffset, yOffset, 12, dialValues[i], 0, 1, 0.01, 0, nil, paramsInList[i][3], paramsInList[i][2])
     
     if ((i % numColumns) == 0) then
       row = row + 1
@@ -108,16 +114,22 @@ end
     
 -- encoder function
 function enc(n, delta)
+    local dialGroupIndex = (listIndex * 2) - 1
+    
     if n == 1 then
-      listIndex = util.clamp(listIndex + util.clamp(delta, -1, 1), 1, 6)
+      dialValues[12] = util.clamp(dialValues[12] + util.clamp(delta, -0.01, 0.01), 0, 1)
+      dials[12]:set_value(dialValues[12])
+      params:set_raw("setAmp", dialValues[12])
     elseif n == 2 then
-      local dialGroupIndex = (listIndex * 2) - 1
-      --params:set(paramsInList[dialGroupIndex][1], 190.0)
-      dials[dialGroupIndex]:set_value_delta(delta)
+      dialValues[dialGroupIndex] = util.clamp(dialValues[dialGroupIndex] + util.clamp(delta, -0.01, 0.01), 0, 1)
+      dials[dialGroupIndex]:set_value(dialValues[dialGroupIndex])
+      params:set_raw(paramsInList[dialGroupIndex][1], dialValues[dialGroupIndex])
     elseif n == 3 then
-      local dialGroupIndex = (listIndex * 2)
-      --params:set(paramsInList[dialGroupIndex][1], 0.0)
-      dials[dialGroupIndex]:set_value_delta(delta)
+      dialGroupIndex = dialGroupIndex + 1 
+      
+      dialValues[dialGroupIndex] = util.clamp(dialValues[dialGroupIndex] + util.clamp(delta, -0.01, 0.01), 0, 1)
+      dials[dialGroupIndex]:set_value(dialValues[dialGroupIndex])
+      params:set_raw(paramsInList[dialGroupIndex][1], dialValues[dialGroupIndex])
     end
       
   screen_dirty = true
@@ -125,17 +137,17 @@ end
 
 -- key function
 function key(n, z)
-  buttons[n] = z
-  local shift = buttons[1]
-
   if (shift == 1) then
     print("shift", n, z)
   else
-    if ((z == 0) and (n == 2)) then
-    elseif ((z == 0) and (n == 3)) then
+    if ((z == 1) and (n == 2)) then
+      listIndex = util.clamp(listIndex - 1, 1, 6)
+    elseif ((z == 1) and (n == 3)) then
+      listIndex = util.clamp(listIndex + 1, 1, 6)
     end
   end
 
+  screen_dirty = true
   redraw()
 end
 
